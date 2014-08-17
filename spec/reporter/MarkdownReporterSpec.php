25c9ef961bba19a052064f589e048eafd08bab05
holyshared <holy.shared.design@gmail.com>
2014-08-17 14:41:42 +0900

    add spec

---
+++ spec/reporter/MarkdownReporterSpec.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of cloak.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+use cloak\reporter\MarkdownReporter;
+
+describe('MarkdownReporter', function() {
+
+    describe('onStart', function() {
+        it('check mock object expectations');
+    });
+
+    describe('onStop', function() {
+        it('output the markdown report');
+        it('check mock object expectations');
+    });
+
+});
