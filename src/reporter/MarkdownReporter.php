a10368ca69437124e59a9166fd76af0410acd6b8
holyshared <holy.shared.design@gmail.com>
2014-08-17 16:03:49 +0900

    add report code

--- src/reporter/MarkdownReporter.php
+++ src/reporter/MarkdownReporter.php
@@ -12,8 +12,12 @@
 namespace cloak\reporter;
 
 use cloak\reporter\Reportable;
+use cloak\Result;
+use cloak\result\File;
+use cloak\writer\FileWriter;
 use cloak\event\StartEventInterface;
 use cloak\event\StopEventInterface;
+
 
 /**
  * Class MarkdownReporter
@@ -23,6 +27,19 @@ class MarkdownReporter implements ReporterInterface
 {
 
     use Reportable;
+
+    /**
+     * @var \cloak\writer\FileWriter
+     */
+    private $reportWriter;
+
+    /**
+     * @param string $outputFilePath
+     */
+    public function __construct($outputFilePath)
+    {
+        $this->reportWriter = new FileWriter($outputFilePath);
+    }
 
     /**
      * @param StartEventInterface $event
@@ -36,6 +53,73 @@ class MarkdownReporter implements ReporterInterface
      */
     public function onStop(StopEventInterface $event)
     {
+        $this->writeMarkdownReport($event->getResult());
+    }
+
+    /**
+     * @param Result $result
+     */
+    private function writeMarkdownReport(Result $result)
+    {
+        $this->writeTitle();
+        $this->writeDescription();
+        $this->writeResult($result);
+    }
+
+    private function writeTitle()
+    {
+        $this->reportWriter->writeLine('# Code Coverage Report');
+        $this->reportWriter->writeLine('');
+    }
+
+    private function writeDescription()
+    {
+        $this->reportWriter->writeLine('Generator: cloak  ');
+        $this->reportWriter->writeLine('Generated at: 2014-07-10 00:00:00  ');
+        $this->reportWriter->writeLine('');
+    }
+
+    private function writeResultHeader()
+    {
+        $this->reportWriter->writeLine('## Result');
+        $this->reportWriter->writeLine('');
+
+        $this->reportWriter->writeLine('| No. | File | Line | Coverage |');
+        $this->reportWriter->writeLine('|:-|:-|-:|-:|');
+    }
+
+    private function writeResult(Result $result)
+    {
+        $this->writeResultHeader();
+
+        $files = $result->getFiles();
+
+        foreach ($files as $key => $file) {
+            $orderNumber = $key + 1;
+            $this->writeFileResult($orderNumber, $file);
+        }
+    }
+
+    private function writeFileResult($orderNumber, File $file)
+    {
+
+        $lineResult = sprintf("%2d/%2d",
+            $file->getExecutedLineCount(),
+            $file->getExecutableLineCount()
+        );
+
+        $coverageResult = sprintf('%6.2f%%', $file->getCodeCoverage()->value());
+
+        $parts = [
+            $orderNumber,
+            $file->getRelativePath(getcwd()),
+            $lineResult,
+            $coverageResult
+        ];
+
+        $record = '|' . implode('|', $parts) . '|';
+
+        $this->reportWriter->writeLine($record);
     }
 
 }
